8ec83a197bf1830d130da41a737ebe5165dbc6c9
Stephen Fudge <sfudge@gmail.com>
2022-07-15 15:07:34 -0400

    changed db file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,14 +13,14 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (id, content) => {
+export const putDb = async (content) => {
   console.log('PUT to the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: id, value: content });
+  const request = store.put({ id:1, value: content });
   const result = await request;
-  console.log(':rocket: - data saved to the database', result);
+  console.log('🚀- data saved to the database', result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
@@ -31,8 +31,14 @@ export const getDb = async () => {
   const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
-  console.log('result.value', result);
-  return result;
+  console.log(result);
+  // console.log('result.value', result);
+  result
+  ? console.log('🚀 - data has been got', result.value)
+  : console.log('🚀 - data not found');
+  return result?.value
+
+  // return result.value;
 };
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'J.A.T.E'
+        title: 'J.A.T.E',
       }),
       new InjectManifest({
         swSrc: './src-sw.js',
